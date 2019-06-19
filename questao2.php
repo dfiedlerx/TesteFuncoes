57d1e182317e3637389d441ed2fcad17ee7388c8
Daniel Fiedler <32338795+dfiedlerx@users.noreply.github.com>
2019-06-19 15:18:30 -0300

    Update questao2.php

--- questao2.php
+++ questao2.php
@@ -1,5 +1,8 @@
 <?php
-
+/*
+Irei simular uma tabela de banco de dados nesta classe com id, Modelo, Placa e Data de entrada
+Será possivel inserir, consultar, deletar, editar, e gerar relatorio completo do estacionamento
+*/
 class Estacionamento {
 
 	private $quantidadeMaxima;
@@ -9,20 +12,77 @@ class Estacionamento {
 	 Vagas preenchidas seguira o modelo:
 		0 =>
 		[
-		    'idCarro' => 0
-			Modelo => 'Ford Ka',
-			Placa => 'WXZ 5452'
-			Data Entrada => '2019/05/02 05:05:05'
+		    	idCarro => 0
+			modelo => 'Ford Ka',
+			placa => 'WXZ 5452'
+			data_entrada => '02/05/2019 05:05:05'
 		]
 	*/
 	private $vagasPreenchidas;
 	
 	public function __construct ($quantidadeMaxima) {
 		
-		$this->$quantidadeMaxima = $quantidadeMaxima;
+		$this->quantidadeMaxima = $quantidadeMaxima;
 		
 	}
 	
+	public function novoCarro ($modelo, $placa) {
 	
+		if (count($vagasPreenchidas) > $quantidadeMaxima) {
+			
+			return 'Estacionamento Lotado.';
+			
+		}
+		
+		$this->vagasPreenchidas[$this->contadorId] = 
+		[
+			'idCarro' => $this->contadorId,
+			'modelo' => $modelo,
+			'placa' => $placa,
+			'dataEntrada' => date ('d/m/Y H:i:s')
+		];
+		
+		$this->contadorId += 1;
+		
+	}
+	
+	public function removeCarro ($idCarro) {
+		
+		unset ($this->vagasPreenchidas[$idCarro]);
+		
+	}
+	
+	/*
+	Retorna um compilado com todos os carros armazenados
+	Eu poderia ter colocado varias coisas em um echo so mas prezei a boa identação do código
+	*/
+	public function relatorioDeCarros () {
+		
+		echo 'Capacidade Máxima: ' , $this->quantidadeMaxima , '<br>';
+		echo 'Quantidade de Carros: ' , count($this->vagasPreenchidas) , '<br><br>';
+	
+		foreach ($this->vagasPreenchidas as $currentVaga) {
+			
+			$this->relatorioCarroIndividual
+			(
+				$currentVaga['idCarro'], 
+				$currentVaga['modelo'], 
+				$currentVaga['placa'], 
+				$currentVaga['dataEntrada']
+			);
+			
+		}
+		
+	}
+	
+	//Como esse trecho é usado em duas ações da classe, tranformei-o em um metodo
+	private function relatorioCarroIndividual ($numeroCarro, $modelo, $placa, $dataEntrada) {
+		
+		echo 'Carro Numero ' , $numeroCarro , '<br>';
+		echo ' - Modelo: ' , $modelo,  '<br>';
+		echo ' - Placa: ' , $placa,  '<br>';
+		echo ' - Data de Entrada: ' , $dataEntrada,  '<br>';
+		
+	}
 	
 }
